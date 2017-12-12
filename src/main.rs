4f3a622fd9966c01fadb5848000ecf1a71026d52
Richard Diamond <wichard@vitalitystudios.com>
2017-12-12 17:02:11 -0600

    Get the correct answer (as giving by the solution key), by really fixing OBO erorrs.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 // A rather brute force approach.
 
 pub fn main() {
-  let mut count = 0u64;
+  let mut count = 1u64;
 
-  for i in 0..5u64 {
+  for i in 0..6u64 {
     for j in 0..17u64 {
       if 493 < 87 * i + 29 * j { break; }
-      count += (493 - 87 * i - 29 * j) / 6;
+      count += (493 - 87 * i - 29 * j) / 6 + 1;
     }
   }
 
