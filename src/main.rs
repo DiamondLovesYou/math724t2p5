0c56669ed649a1b3415e86ed0a0972d4a89b6535
Richard Diamond <wichard@vitalitystudios.com>
2017-12-13 07:41:35 -0600

    Another crude algo to calculate change for a three dollar bill as in problem #2 on the final.

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,23 @@ pub fn main() {
     }
   }
 
-  println!("count = {}", count);
+  println!("Harry Potter change (Test #2): count = {}", count);
+
+  let mut count = 0u64;
+
+  for i in 1..10u64 {
+    for j in 1..10u64 {
+      if i * 25 + j * 10 > 300 { break; }
+      for k in 1..10u64 {
+        let amount = i * 25 + j * 10 + k * 5;
+        if amount > 300 { break; }
+
+        if 300 - amount > 10 { continue; }
+
+        count += 1;
+      }
+    }
+  }
+
+  println!("Three dollar bill change (Final): count = {}", count);
 }
